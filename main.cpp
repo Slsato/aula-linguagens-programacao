c398b381e25b36259ee5ed8e87d9c83a0171f619
Slsato <162244183+Slsato@users.noreply.github.com>
2024-04-05 20:37:51 -0400

    Altura e Idade em C++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float altura;
+    int idade;
+
+    cin >> altura;
+    cin >> idade;
+    cout << "a altura eh: " << altura << endl;
+    cout << "a idade eh: " << idade << endl;
+
+    return 0;
+}
